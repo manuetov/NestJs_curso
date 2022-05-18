eab9d9fa2eb7933fa7f3512a621ae43229865fe1
manuetov <manuetov@hotmail.com>
2022-05-18 14:44:06 +0200

    server create

---
+++ src/product/dto/interfaces/product.interface.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+// se crearán las interfaces que se van a usar con typescript para recibir ayuda como autocompletado, análisis de errores.. etc.
---
+++ src/product/dto/product.dto.ts
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+// aquí se definirá los que se envia entre el cliente y el servidor
+
