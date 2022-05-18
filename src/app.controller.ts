212de213c3cf1c78f1c3371631c04595d4c1fc45
manuetov <manuetov@hotmail.com>
2022-05-18 16:22:31 +0200

    ruta post con postman

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Controller, Get, Post, Res, HttpStatus, Body } from '@nestjs/common';
 
-@Controller()
+import { CreateProductDTO } from './product/dto/product.dto'
+@Controller('product')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/create')
+  createPost(@Res() res, @Body() createProductDTO: CreateProductDTO){
+    //defino la instancia de la clase importada
+    console.log(createProductDTO);
+    return res.status(HttpStatus.OK).json({
+      message: 'ruta post'
+    });
   }
 }
--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,4 +1,12 @@
 /* eslint-disable prettier/prettier */
 // eslint-disable-next-line prettier/prettier
 // aquí se definirá los que se envia entre el cliente y el servidor
+export class CreateProductDTO {
+   readonly name: string;
+   readonly brand: string;
+   readonly description: string;
+   readonly imageURL: string;
+   readonly price: number;
+   readonly createdAt: Date;
 
+}
